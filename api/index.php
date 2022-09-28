e6f40bb41436e309ad1e3a8c4e82eee9a66c2682
Muzaffer Övün <83140937+muzafferovun@users.noreply.github.com>
2022-09-29 00:38:31 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -14,7 +14,10 @@ $action="";
 $apipart=explode("_",$api);
 $modul=$apipart[0];
 $action=$apipart[1];
+$moduleRouting=true;
 //modul klasör ve dosyalarının otomatik oluşturulması
 if($modul=="generateModul") include("core/moduleGenerator/generate.php");
-include("moduls/$modul/controllers/index.php");
+if($moduleRouting){
+  include("moduls/$modul/controllers/index.php");
+}
 ?>
