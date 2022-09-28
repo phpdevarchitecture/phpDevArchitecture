03fdb2dd2a9d7d3acefd5c4cee63bc1af6ecdce0
Muzaffer Övün <83140937+muzafferovun@users.noreply.github.com>
2022-09-28 23:04:53 +0300

    Update controllers

--- api/moduls/user/controllers/index.php
+++ api/moduls/user/controllers/index.php
@@ -1,9 +1,14 @@
 <?
 switch ($action) {
-    case "addUser":
+  case "addUser":
     include("moduls/$modul/controllers/addUser.php");
+  break;
+  case "getUser":
     include("moduls/$modul/controllers/getUser.php");
+  break;
+  case "getUsers":
     include("moduls/$modul/controllers/getUsers.php");
-      break;
+  break;
+
 }
 ?>
