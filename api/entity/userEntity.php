48af19af4b9f1300ae01357b27187a61f261f4a7
Muzaffer Övün <83140937+muzafferovun@users.noreply.github.com>
2022-09-29 00:13:40 +0300

    Update userEntity.php

--- api/entity/userEntity.php
+++ api/entity/userEntity.php
@@ -4,16 +4,13 @@ class User extends Users {
   private $id=new EntityItem("id","id","int","0");
   private $username=new EntityItem("username","username","string","50");
   private $password=new EntityItem("password","password","string","100");
-
-
-
    function User() {
      this->admin.setPermission(true,true,true,true);
      this->editor.setPermission(true,true,true,true);
      this->moderator.setPermission(true,true,true,true);
      this->guest.setPermission(false,false,true,false);
      this->owner.setPermission(true,true,true,true);
-
+     this->id->setIsIdentity(true);
      this->username->setIsNull(false);
      this->username->setIsUnique(true);
      this->password->setIsNull(false);
